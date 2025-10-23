d2c31be2564b481463467733b6619cee64d58c29
geronimo12322 <geronimo123@gmail.com>
2025-10-23 17:22:17 -0300

    mysqli

--- linkDB.php
+++ linkDB.php
@@ -1,25 +1,15 @@
 <?php
-class datos {
-    var $host = "localhost";
-    var $db = "databaserappifarma";
-    var $usuario = "root";
-    var $contraseña = "";
-}
-
-class database {
-    function conexion(){
-        $datos = new datos();
-        $host = $datos->host;
-        $db = $datos->db;
-        $usuario = $datos->usuario;
-        $contraseña = $datos->contraseña;
-        try {
-            $cnn = new PDO('mysql:host=' . $host . ';dbname=' . $db, $usuario, $contraseña);
-            return $cnn;
-        } catch (PDOException $e) {
-            print "¡Error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
+function getConnection(): mysqli {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "databaserappifarma";
+    
+    $conn = new mysqli($servername, $username, $password, $database);
+    
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
     }
+    return $conn;
 }
 ?>
